213b65b517cddd03a5a36d6ef15bb78bf573e755
Miloni Gada <miloniketan@gmail.com>
2024-04-06 17:36:08 +0100

    Deleted the Excel Acrobat file, Added Iwaniuk_etal_2001_Table 1 - Snapshot,  ReadMe, R script. Added Data Readable  and Online Data Base to Iwaniuk_etal_2001_Table 2.ReadMe. And added  Iwaniuk_etal_2001_Table3_snapshot
    Deleted the Excel Acrobat file we created yesterday. Added Iwaniuk_etal_2001_Table 1 - Snapshot,  ReadMe, R script. Added Data Readable Part and Online Data Base to Iwaniuk_etal_2001_Table 2.ReadMe. Added  Iwaniuk_etal_2001_Table3_snapshot.

---
+++ Iwaniuk_etal_2001/Iwaniuk_etal_2001_Table1.R
@@ -0,0 +1,30 @@
+#1. Source
+setwd("C:/Users/MILONI/OneDrive - University of Bath/Research Schemes/Allen Institue/Evo-M1-Trait-Data/")
+
+#2. Table 1
+
+## 1. Read direct from xl
+library(readxl)
+folder_path <- "./Iwaniuk_etal_2001/"
+tabledirectxl <- read_excel(paste0(folder_path,"Iwaniuk_etal_2001_Table1_snapshot.xlsx"))
+
+#3. Save
+
+# Finalize dataframe (UPDATE!!!)
+final.dataframe <- tabledirectxl
+
+# Get Item name: Get Path of the current script, Extract the file name, Remove the ".R" extension
+library(rstudioapi)
+item_name <- gsub("\\.R$", "", basename(rstudioapi::getActiveDocumentContext()$path))
+
+# Get Item encoded
+library(readxl) 
+filecodes <- read_excel("./__ReadMe.xlsx", sheet = "Sheet1")
+item_encoded <- filecodes$"Item encoded"[match(item_name, filecodes$"Item name")]
+
+# Save dataframe to a CSV file
+write.csv(final.dataframe, file = paste0(folder_path, item_name, ".csv"), row.names = FALSE)
+
+# Save dataframe to a TSV file in the online database
+tsv_file_path <- "./__Public/comparative-data/"
+write.table(final.dataframe, file = paste0(tsv_file_path, item_encoded, ".tsv"), sep = "\t", row.names = FALSE)
---
+++ Iwaniuk_etal_2001/Iwaniuk_etal_2001_Table1.ReadMe.md
@@ -0,0 +1,20 @@
+Source
+
+pdf (paywall)
+
+--> Snapshot
+
+Manually entered data from "Table 1: Encephalization Quotients (EQs) and Play Scores for 15 
+Mammalian Orders"
+
+Iwaniuk_etal_2001_Table1_snapshot.xlsx
+
+--> Data readable 
+
+Iwaniuk_etal_2001_Table1.csv <-- USE THIS
+
+--> Online database
+
+Added TSV copy named with DOI to https://github.com/r03ert0/comparative-data
+
+10.1037%2F0735-7036.115.1.29_Table1.tsv <-- ONLINE COPY
